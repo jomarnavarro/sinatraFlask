c1431668bfb77ca1908ec0e5958b8fc2fc7cb5d7
Omar Navarro <jomarnavarro@gmail.com>
2020-10-17 08:57:06 -0500

    Implements the index page, flashing confirmation messages and displaying et creating songs

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -38,9 +38,8 @@ Session(app)
 
 
 @app.route("/")
-@login_required
 def index():
-    return apology("TODO", 404)
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -55,17 +54,20 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            flash("Error - You must provide a username")
+            return render_template("login.html")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            flash("Error - You must provide a username")
+            return render_template("login.html")
 
         rows = db_helpers.query_username(request.form.get('username'))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            flash("Error - invalid username and/or password")
+            return render_template("login.html")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -74,6 +76,7 @@ def login():
         user_type = db_helpers.get_user_type(session['user_id'])
         session['user_type'] = user_type
         # Redirect user to home page
+        flash(f"Welcome {request.form.get('username')}")
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
@@ -85,9 +88,15 @@ def login():
 def logout():
     """Log user out"""
 
+    user_id = session["user_id"]
+
+    user_name = db_helpers.get_username_by_id(user_id)[0]['username']
+    print(user_name)
+    
+
     # Forget any user_id
     session.clear()
-
+    flash(f"Goodbye {user_name}")
     # Redirect user to login form
     return redirect("/")
 
@@ -102,11 +111,14 @@ def register():
         password = request.form.get('password')
         repeat_password = request.form.get('repeat_password')
         if not username:
-            return apology('You did not enter a username.', 403)
+            flash("Error - You must enter a username")
+            return render_template("register.html")
         if not password or not repeat_password:
-            return apology('You did not enter a password.', 403)
+            flash("Error - You must enter a password that matches")
+            return render_template("register.html")
         if password != repeat_password:
-            return apology('Passwords do not match.', 403)
+            flash('Passwords do not match.')
+            return render_template("register.html")
         # if not meets_complexity(password):
         #     return apology(
         #         'Password must: \n\t-be 8+ characters long.\n\t-contain uppercase and lowercase letters\n\t-a number '
@@ -116,11 +128,12 @@ def register():
         rows = db_helpers.query_username(username)
 
         if len(rows) != 0:
-            return apology(f"{username} is already taken", 403)
+            flash(f"User '{username}' is already taken")
+            return render_template("register.html")
 
         db_helpers.insert_user(username, generate_password_hash(password), 'user')
-
-        return redirect("/login")
+        flash(f"{username} was succesfully registered.")
+        return render_template("login.html")
 
 
 @app.route("/about")
@@ -138,12 +151,14 @@ def contact():
         email = request.form.get('email')
         message = request.form.get('message')
         if not email:
-            return apology('You need to enter an email', 402)
+            flash("You need to enter an email")
+            return render_template("contact.html")
         if not message:
-            return apology('You need to enter a message', 402)
+            flash("You need to enter a message")
+            return render_template("contact.html")
 
         db_helpers.insert_message(name, email, message)
-
+        flash("Message received.")
         return redirect('/')
 
 
@@ -159,7 +174,8 @@ def list_song_number(song_number):
     # get all songs from the database
     row = db_helpers.fetch_song_info(song_number)
     if not row:
-        return apology(f"Song {song_number} does not exist", 404)
+        flash(f"Song {song_number} does not exist.")
+        return render_template("songs.html")
     
     return render_template('song.html', song_info=row)
 
@@ -168,7 +184,57 @@ def list_song_number(song_number):
 def like_song(song_number):
     # update number of likes
     db_helpers.update_number_likes(song_number)
+    flash("Thank you for your like")
     return redirect(f"/songs/{song_number}")
+
+
+@app.route("/songs/new", methods=["GET", "POST"])
+def create_song():
+    if request.method == "GET":
+        return render_template("song_form.html")
+    else:
+        # fetch form data
+        messages = []
+        artist = request.form.get("artist")
+        country = request.form.get("country")
+        title = request.form.get("title")
+        duration = request.form.get("duration")
+        lyrics = request.form.get("lyrics")
+        
+        if not artist:
+            messages.append("Your song has no artist name")
+        if not country:
+            country = ''
+            messages.append("Your song has no country for artist")
+        if not title:
+            messages.append("Your song needs a title")
+        if not duration:
+            duration = 0
+            messages.append("Your song has no duration time")
+        if not lyrics:
+            lyrics = ''
+            messages.append("Your song has no lyrics")
+
+        if not artist and not title and not country:
+            for msg in messages:
+                flash(msg)
+            return render_template("song_form.html")
+        print(f"before getting artist_id, title is {title}")
+        # get the artist id, whether it exists or is inserted on the spot.
+        artist_id = db_helpers.get_artist_id(artist, country)
+        print(f"after getting artist_id, title is {title}")
+        print(artist_id)
+        # get the song_id, whether it exists already or is added
+        
+        song_id, existed = db_helpers.get_song_id(title, duration, artist_id, lyrics)
+        print(song_id)
+        if existed:
+            flash(f"Song {title} already existed in the DB.  Pls edit it.")
+        # insert song into database
+        # get song id
+        # redirect user to /song/id
+        return redirect(f"/songs/{song_id}")
+
 
 def errorhandler(e):
     """Handle error"""
--- db_helpers.py
+++ db_helpers.py
@@ -13,6 +13,14 @@ def query_username(username):
     return c.fetchall()
 
 
+def get_username_by_id(uid):
+    """Get the username by the id"""
+    username_select = "SELECT username FROM users WHERE id=?"
+    test = (uid,)
+    c.execute(username_select, test)
+    return c.fetchall()
+
+
 def insert_user(username, password, user_type):
     """Insert User into the database"""
     user_insert_select = "INSERT INTO users (username, hash, user_type) VALUES (?, ?, ?)"
@@ -63,3 +71,46 @@ def update_number_likes(song_number):
     test = (num_likes + 1, song_number)
     c.execute(update_likes, test)
     conn.commit()
+
+
+def get_artist_id(artist, nationality):
+    """Gets the artist id, whether it exists or has to insert it"""
+    artist_id_select = "SELECT id FROM artist WHERE name = ? and nationality = ?"
+    test = (artist, nationality)
+    c.execute(artist_id_select, test)
+    row = c.fetchone()
+    if row is None:
+        # insert the motherfucker
+        # TODO get the image URL
+        # TODO if no image is found, img_url = '', 
+        # TODO send a message to the user.
+        img_url = ''
+        artist_insert = "INSERT INTO artist (name, nationality, img_url) VALUES (?, ?, ?)"
+        test2 = (artist, nationality, img_url)
+        c.execute(artist_insert, test2)
+        conn.commit()
+        
+        # retry the select,
+        c.execute(artist_id_select, test)
+        row = c.fetchone()
+    return row['id']
+
+def get_song_id(title, duration, artist_id, lyrics):
+    song_id_select = "SELECT id FROM songs WHERE title = ? and artist_id = ?"
+    test = (title, artist_id)
+    c.execute(song_id_select, test)
+    row = c.fetchone()
+    if not row:
+        # insert the motherfucker
+        print(title)
+        song_insert = "INSERT INTO songs (title, duration, artist_id, lyrics, num_likes) VALUES (?, ?, ?, ?, ?)"
+        test2 = (title, duration, artist_id, lyrics, 0)
+        c.execute(song_insert, test2)
+        conn.commit()
+         
+        #retry the select
+        c.execute(song_id_select, test)
+        row = c.fetchone()
+        return row['id'], False
+    else:
+        return row['id'], True
